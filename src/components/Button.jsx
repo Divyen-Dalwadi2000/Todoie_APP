e92cdec504c428f81d22dede815c370c1686c1d9
Mrdivyendalwadi <114602971+Divyen-Dalwadi2000@users.noreply.github.com>
2024-04-21 17:23:08 +0530

    create a multiple Static compoonents and design the todo box

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import React from "react"
 export default function Button()
 {
     return(
-        <button>Add ToDo </button>
+        <button className="bg-blue-500 p-1 mt-2 rounded hover:bg-blue-700 hover:text-white ">Add ToDo</button>
     )
 
 }
